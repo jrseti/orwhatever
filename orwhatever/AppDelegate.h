8ca3aeaa01a4d5beaebcee241d4d39c1a7184918
Jon Richards <richards@inreach.com>
2013-07-12 17:44:38 -0700

    Initial Commit

---
+++ orwhatever/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  orwhatever
+//
+//  Created by jrichards on 7/12/13.
+//  Copyright (c) 2013 MoversAndShakers.mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ orwhatever/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  orwhatever
+//
+//  Created by jrichards on 7/12/13.
+//  Copyright (c) 2013 MoversAndShakers.mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
